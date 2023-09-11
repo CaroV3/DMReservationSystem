baab9c5092bb8448a7265cb1ff6ad76f150dddda
carol <caroline.verbeek@ziggo.nl>
2023-09-11 16:20:37 +0200

    home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.web')
+
+@section('title', 'Home')
+
+@section('main-content')
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum debitis doloremque esse iusto nemo provident reprehenderit repudiandae? Aspernatur corporis eaque, et expedita fugiat modi, necessitatibus numquam pariatur quia, vero vitae?</p>
+@endsection
